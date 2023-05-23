29cce27c89e8b601a9dbc1c141c6dd47e27c723a
kemboi590 <bkemboi590@gmail.com>
2023-05-23 10:59:38 +0300

    fetched data to the console

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Nairobi';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c91f9666e1msha2a78962a9ff863p1255ffjsnc55172833efb',
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+      
+    }
+    fetchData()
+        
+  },[])
 
 
   return (
     <div>
       <h1>Welcome To Weather Application</h1>
+
+  
       
       
     </div>
