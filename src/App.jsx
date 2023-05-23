65eaf72cf4444461f01f25685398b83a977453ce
kemboi590 <bkemboi590@gmail.com>
2023-05-23 10:35:13 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Welcome To Weather Application</h1>
+      
+      
+    </div>
+  )
+}
+
+export default App
