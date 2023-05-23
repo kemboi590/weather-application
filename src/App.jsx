5c03ea273efc1686d3ee8c13b2d8e1b427a6fcb3
MuneneCalvin <munenecalvn@gmail.com>
2023-05-23 12:01:44 +0300

    Input func

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { useState, useEffect} from 'react'
 import './App.css'
 
 function App() {
+  const [location, setlocation] = useState('');
+
   useEffect(() => {
     const fetchData = async () => {
-      const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Nairobi';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c91f9666e1msha2a78962a9ff863p1255ffjsnc55172833efb',
-		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+      const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=${location}';
+      const options = {
+	      method: 'GET',
+	      headers: {
+		      'X-RapidAPI-Key': 'c91f9666e1msha2a78962a9ff863p1255ffjsnc55172833efb',
+		      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
 	}
 };
 
@@ -25,17 +27,23 @@ try {
     }
     fetchData()
         
-  },[])
+  },[location])
+
+  const handleSearch = (e) => {
+    setlocation(e.target.value);
+    e.preventDefault();
+  }
 
 
   return (
     <div>
       <h1>Welcome To Weather Application</h1>
-
-  
-      
-      
+      <form action="handleSearch">
+        <input type="text" placeholder='Enter Location' value={location} onChange={handleSearch} />
+        <button type='submit'>Get Weather</button>
+      </form>
     </div>
+
   )
 }
 
